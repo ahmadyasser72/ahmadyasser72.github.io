629827c81e6bb336b7169d6469b3a5845f1a6e1a
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2024-12-16 11:27:07 +0800

    konfigurasi github actions untuk deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ahmadyasser72.github.io",
   integrations: [UnoCSS({ injectReset: true })],
 });
